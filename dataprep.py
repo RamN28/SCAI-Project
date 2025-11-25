0576665cce5927cd611975c18d2c98783d824ea7
Kyle <kylin2@s.sfusd.edu>
2025-11-25 13:43:59 -0800

    some changes to dataprep

--- dataprep.py
+++ dataprep.py
@@ -3,18 +3,24 @@ import pandas as pd
 from torch.utils.data import Dataset
 
 class MeatDataset(Dataset):
-  def __init__(self, image_paths, environmental_data, labels):
-    self.image_paths = image_paths
-    self.env_data = environmental_data #[temperature, time_since_slaughter, location_data]
-    self.labels = labels
+    def __init__(self, image_paths, environmental_data, labels):
+        self.image_paths = image_paths
+        self.env_data = environmental_data  # [temperature, time_since_slaughter, location_data]
+        self.labels = labels
 
-  def __len__(self):
-    return len(self.labels)
+    def __len__(self):
+        return len(self.labels)
 
-def __getitem__(self, idx):
-  image = load_and_preprocess_image (self.image_paths[idx])
-  env_features = torch.tensor (self.env_data[idx], dtype = torch.float3
+    def __getitem__(self, idx):
+        # You must define this function somewhere
+        image = load_and_preprocess_image(self.image_paths[idx])
 
-  label = torch.tensor (self.labels[idx], dtype = torch.long)
-  return image, env_featuers, label
+        # Convert environmental features to a tensor
+        env_features = torch.tensor(self.env_data[idx], dtype=torch.float32)
+
+        label = torch.tensor(self.labels[idx], dtype=torch.long)
+
+        return image, env_features, label
+
+
 
