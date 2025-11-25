403ab65b1c3ec1d2d8d12fdff5000d3643bc395b
AyanDasguptaCoding <ayandasgupta0@gmail.com>
2025-11-25 12:40:12 -0800

    Update dataLoaderSetup.py

--- dataLoaderSetup.py
+++ dataLoaderSetup.py
@@ -1,6 +1,6 @@
 import os
 import torch
-from torch.utils.data import Dataset, DataLoader  # Fixed "Datset" typo
+from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
 
@@ -36,21 +36,21 @@ class MeatFreshnessDataset(Dataset):
         
         return image, label
 
-def get_transforms():  # Fixed indentation
+def get_transforms():
     train_transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.RandomHorizontalFlip(0.3),
         transforms.RandomRotation(10),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                           std=[0.229, 0.224, 0.225])
+                             std=[0.229, 0.224, 0.225])
     ])
     
     test_transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                           std=[0.229, 0.224, 0.225])
+                             std=[0.229, 0.224, 0.225])
     ])
     
     return train_transform, test_transform
