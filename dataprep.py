b544bf1b7f652f7b05f7a77222e44a10a021a915
6elax <goldenpoodle2007@gmail.com>
2025-12-01 15:53:30 -0800

    edited dataprep.py

--- dataprep.py
+++ dataprep.py
@@ -1,6 +1,8 @@
 import torch
 import pandas as pd
 from torch.utils.data import Dataset
+from PIL import Image
+from torchvision import transforms
 
 class MeatDataset(Dataset):
     def __init__(self, image_paths, environmental_data, labels):
@@ -11,10 +13,16 @@ class MeatDataset(Dataset):
     def __len__(self):
         return len(self.labels)
 
+    def load_and_preprocess_image(image_path):
+        transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor()])
+        img = Image.open(image_path).convert("RGB")
+        return transform(img)
+
     def __getitem__(self, idx):
         # You must define this function somewhere
         image = load_and_preprocess_image(self.image_paths[idx])
-
         # Convert environmental features to a tensor
         env_features = torch.tensor(self.env_data[idx], dtype=torch.float32)
 
