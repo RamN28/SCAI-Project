103cc4ffc7bc80fd29563bea696569c97beaacb2
6elax <goldenpoodle2007@gmail.com>
2025-12-01 19:17:48 -0800

    edited files in model1

--- model1/simpleNN.py
+++ model1/simpleNN.py
@@ -4,11 +4,18 @@ import torch.nn as nn #neural network section of torch
 class myNN(nn.Module):
 
     def __init__(self): #for layers
-        self.layer1 = nn.Linear(2, 3)  #lets say we have 2 hidden layers 👍
-                #takes in certain amt of inputs and outputs
-        self.layer2 = nn.Linear(3, 2)  # 3 inputs, 2 outputs
-        self.layer3 = nn.Linear(2, 1)  # 2 inputs, 1 output
-        #too many layers may be too much computing or overfit data
+        super(myNN, self).__init__()
+
+        # use convolutional layers (for images)
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)   # 3 color channels
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+
+        # Reduce image size
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # Fully Connected Layers
+        self.fc1 = nn.Linear(32 * 56 * 56, 64)  # after pooling twice
+        self.fc2 = nn.Linear(64, 3)             # 3 classes output
         
         self.relu = nn.ReLU()  #activation! Defines RELU function, more common 
         
@@ -17,17 +24,17 @@ class myNN(nn.Module):
     #now need to call all the methods
 
     def forward(self, input): #for activation
-        #getting an input, just need to return output of function
-        result = self.layer1(input)
-        result = self.relu(result)	#need to make sure result1 are nonlinear
 
-        result = self.layer2(result)
-        result = self.relu(result)
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
 
-        result = self.layer3(result)
-        # dont need a relu at the end, since output values won’t go into anything, doesn’t need to be nonlinear
+        # Flatten tensor
+        x = x.view(x.size(0), -1)
 
-        return result
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
 
 
 ########################################
